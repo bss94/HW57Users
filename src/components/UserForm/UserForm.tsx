cb9695ec5c9a1792155cb909fcb9a94fbf85e132
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-06-07 04:04:25 +0600

    complete UserForm visual,add state,create functions to edit state on changes form fiels,add dinamic class to UserItem for change color to user.isActive

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,10 +1,86 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {User} from '../../types';
 
-const UserForm = () => {
+interface Props{
+
+}
+const UserForm:React.FC<Props> = ({}) => {
+    const [userForm,setUserForm]=useState<User>({
+        name:'',
+        email:'',
+        isActive:false,
+        role:'',
+    });
+    const changeUser=(event:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>)=>{
+        setUserForm((prevState) =>({
+            ...prevState,
+            [event.target.name]:event.target.value,
+        }));
+    };
+    const changeActive=(event:React.ChangeEvent<HTMLInputElement>)=>{
+        setUserForm((prevState) =>({
+        ...prevState,
+        [event.target.name]:!prevState.isActive,
+    }));
+    }
+
+
     return (
-        <div>
-            UserForm
-        </div>
+        <form>
+            <h4>add new user</h4>
+            <div className="form-group text-start my-3">
+                <label htmlFor="name">Name</label>
+                <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    value={userForm.name}
+                    className='form-control'
+                    onChange={changeUser}
+                />
+            </div>
+            <div className="form-group text-start my-3">
+                <label htmlFor="email">Email</label>
+                <input
+                    name='email'
+                    id='email'
+                    value={userForm.email}
+                    className='form-control'
+                    onChange={changeUser}
+                />
+            </div>
+            <div className="form-group text-start my-3">
+                <label htmlFor="email">Role</label>
+                <select
+                    name="role"
+                    id="role"
+                    value={userForm.role}
+                    className="form-control"
+                    onChange={changeUser}
+                >
+                    <option value={''}>select user</option>
+                    <option value={'user'}>user</option>
+                    <option value={'admin'}>admin</option>
+                    <option value={'editor'}>editor</option>
+                </select>
+            </div>
+            <div className="form-group text-start">
+                <div className="form-check my-3 ">
+                    <label className="form-check-label px-3" htmlFor="isActive">Active user</label>
+                    <input
+                        type="checkbox"
+                        name="isActive"
+                        id="isActive"
+                        checked={userForm.isActive}
+                        className="form-check-input "
+                        onChange={changeActive}
+                    />
+                </div>
+            </div>
+
+            <button type="submit" className={'btn btn-primary mt-2'}>Create</button>
+
+        </form>
     );
 };
 
--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -5,12 +5,19 @@ interface Props{
     user:User
 }
 const UserItem:React.FC<Props> = ({user}) => {
+    const isActiveClasses:string[]=['card-text','small']
+    if(user.isActive){
+        isActiveClasses.push('text-success')
+    }else{
+        isActiveClasses.push('text-danger')
+    }
+
     return (
         <div className={'card mb-2'}>
             <div className="row g-0">
                 <h6 className={'card-title'}>User name: {user.name}</h6>
                 <p className={'card-text'}>Email: {user.email}</p>
-                <p className={'card-text small'}>Active: {user.isActive.toString()}</p>
+                <p className={isActiveClasses.join(' ')}>Active: {user.isActive.toString()}</p>
                 <p className={'card-text'}>Role: {user.role}</p>
             </div>
 
